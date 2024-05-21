39822329ed0192b4171fd9f369eea20f4c69ae68
Giorgi Mamaladze <mamaladzegio99@gmail.com>
2024-05-21 19:12:45 +0400

    setup dir-s

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { HomeComponent } from './home/home.component';
+import { RoomsComponent } from './rooms/rooms.component';
+import { HotelsComponent } from './hotels/hotels.component';
+import { BookedRoomsComponent } from './booked-rooms/booked-rooms.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationComponent,
+    HomeComponent,
+    RoomsComponent,
+    HotelsComponent,
+    BookedRoomsComponent
   ],
   imports: [
     BrowserModule,
